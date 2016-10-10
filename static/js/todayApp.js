4b25594dd8a0ec7d30a84801f9e9b661592cc1b6
zoreet <zoreetdotcom@gmail.com>
2016-10-10 21:13:25 +0200

    these files are getting to big; move the logic into separate files

---
+++ static/js/todayApp.js
@@ -0,0 +1,144 @@
+var todayApp = {
+	/*
+		_______      _________________
+		`MM'`MM\     `M'`MM'MMMMMMMMMM
+		 MM  MMM\     M  MM /   MM   \
+		 MM  M\MM\    M  MM     MM
+		 MM  M \MM\   M  MM     MM
+		 MM  M  \MM\  M  MM     MM
+		 MM  M   \MM\ M  MM     MM
+		 MM  M    \MM\M  MM     MM
+		 MM  M     \MMM  MM     MM
+		 MM  M      \MM  MM     MM
+		_MM__M_      \M _MM_   _MM_
+	*/
+
+	init: function(){
+
+		todayData.get();
+
+		todayApp.display();
+
+
+
+
+		$('#new_todo_item').on('keypress',function(e){
+			if(e.which == 13) { // enter key
+				e.preventDefault();
+				if(todayApp.addItem( $.trim( $(this).html() ) ) ) {
+					$(this).html("");
+				}
+			}
+		});
+		$('#todos')
+			.on('click','.todo_item__remove',function(e){
+				e.preventDefault();
+				todayApp.removeItem( $(this).data('id') );
+			})
+			.on('click','.todo_item__check',function(e){
+				e.preventDefault();
+				todayApp.toggleStatus( $(this).data('id') );
+			})
+			.on('keypress', '.input', function(e){
+				if(e.which == 13) { // enter key
+					e.preventDefault();
+				}
+			})
+			.on('input', '.input', function(e){
+				todayApp.updateItem( $(this).data('id'), $.trim( $(this).html() ) );
+			})
+
+		$('.previousDay').click(function(e){
+			e.preventDefault();
+			todayApp.shiftDate( -1 );
+		});
+		$('.today').click(function(e){
+			e.preventDefault();
+			todayApp.shiftDate( 0 );
+		})
+	},
+
+	/*
+		___       ___       _      _______      ___
+		`MMb     dMM'      dM.     `MM'`MM\     `M'
+		 MMM.   ,PMM      ,MMb      MM  MMM\     M
+		 M`Mb   d'MM      d'YM.     MM  M\MM\    M
+		 M YM. ,P MM     ,P `Mb     MM  M \MM\   M
+		 M `Mb d' MM     d'  YM.    MM  M  \MM\  M
+		 M  YM.P  MM    ,P   `Mb    MM  M   \MM\ M
+		 M  `Mb'  MM    d'    YM.   MM  M    \MM\M
+		 M   YP   MM   ,MMMMMMMMb   MM  M     \MMM
+		 M   `'   MM   d'      YM.  MM  M      \MM
+		_M_      _MM__dM_     _dMM__MM__M_      \M
+	*/
+
+	addItem: function( text ) {
+		text = $.trim(String(text));
+		id = $.now(),
+		status = false;
+
+		if(text !== "") {
+			todayData.items[id] = {
+				'text': text,
+				'status': status
+			};
+
+			todayData.save();
+			todayApp.display();
+
+			return true;
+		}
+
+		return false;
+	},
+	display: function() {
+		$('h1').html( moment(todayData.today).format('dddd, MMMM Do, YYYY') );
+
+		var $todos = $('#todos');
+		var items = todayData.items;
+		var newMarkup = "";
+
+		for(item in items) {
+			id = item;
+			text = items[item].text;
+			status = items[item].status;
+
+			newMarkup+= "<li class='todo_item " + status + "' id='" + id + "'><a href='' class='todo_item__check' data-id='" + id + "'>&#10003;</a><div class='input' contenteditable data-id='" + id + "'>" + text + "</div><a href='' class='todo_item__remove' data-id='" + id + "'>x</a></li>";
+		}
+
+		$todos.html(newMarkup);
+	},
+	removeItem: function( id ) {
+		delete todayData.items[id];
+		todayData.save();
+		todayApp.display();
+	},
+	shiftDate: function( position ) {
+		todayData.shiftDate( position );
+		todayData.get();
+		todayApp.display();
+	},
+	toggleStatus: function( id ) {
+		todayData.items[id].status = !todayData.items[id].status;
+		todayData.save();
+		todayApp.display();
+	},
+	updateItem: function( id, text ) {
+		todayData.items[id].text = text;
+		todayData.save();
+	},
+
+	/*
+		__________    ____       ____    ____       ____
+		MMMMMMMMMM   6MMMMb     6MMMMb   `MM'      6MMMMb\
+		/   MM   \  8P    Y8   8P    Y8   MM      6M'    `
+		    MM     6M      Mb 6M      Mb  MM      MM
+		    MM     MM      MM MM      MM  MM      YM.
+		    MM     MM      MM MM      MM  MM       YMMMMb
+		    MM     MM      MM MM      MM  MM           `Mb
+		    MM     MM      MM MM      MM  MM            MM
+		    MM     YM      M9 YM      M9  MM            MM
+		    MM      8b    d8   8b    d8   MM    / L    ,M9
+		   _MM_      YMMMM9     YMMMM9   _MMMMMMM MYMMMM9
+	*/
+}
