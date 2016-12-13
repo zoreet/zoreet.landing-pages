431ed3dee6c4ddd57a97eab1eb6d8e92887d5c95
zoreet <zoreetdotcom@gmail.com>
2016-12-13 21:22:11 +0100

    well if i expose the path in every request then how can i protect it :)); also return a message if I can't save the data

--- saveData.php
+++ saveData.php
@@ -2,9 +2,19 @@
 	$id = $_POST['dateID'];
 	$todos = $_POST['todos'];
 	$user = $_SERVER['PHP_AUTH_USER'];
-	file_put_contents(getcwd() . '/data/' . $user . '/' . $id . '.txt', $todos);
-	echo getcwd() . '/data/' . $user . '/' . $id . '.txt';
-	echo $_POST['todos'];
-
-	// var_dump($_POST);
+	$result = file_put_contents(getcwd() . '/data/' . $user . '/' . $id . '.txt', $todos);
+	if($result !== false) {
+		//success
+		header("Status: 200");
+		echo json_encode(array(
+        	result => "Data saved!",
+        	code => 200
+    	));
+	} else {
+		// fail
+		echo json_encode(array(
+        	result => "I can't write to that file.",
+        	code => 502
+    	));
+	}
 ?>
