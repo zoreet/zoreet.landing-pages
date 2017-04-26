3c26adb3917734261901c31f94bc97504cb83efc
zoreet <zoreetdotcom@gmail.com>
2017-04-26 23:32:34 +0200

    fix the favicons; now adding to apple homescreen works

--- index.php
+++ index.php
@@ -6,15 +6,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="static/css/style.css">
 
-	<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-	<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
-	<link rel="manifest" href="/manifest.json">
-	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffffff">
-	<meta name="theme-color" content="#ffffff">
-
-	<link rel="manifest" href="manifest.json">
-	<link rel="mask-icon" href="safari-pinned-tab.svg" color="#5bbad5">
+	<link rel="icon" type="image/png" href="/static/img/favicon/favicon-16x16.png" sizes="16x16">
+	<link rel="apple-touch-icon" sizes="180x180" href="/static/img/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/static/img/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/static/img/favicon/favicon-16x16.png">
+	<link rel="manifest" href="/static/img/favicon/manifest.json">
+	<link rel="mask-icon" href="/static/img/favicon/safari-pinned-tab.svg" color="#29e5c9">
 	<meta name="theme-color" content="#ffffff">
 </head>
 <body>
