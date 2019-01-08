e290c0edfa46710c7a459523616199cbe56c95ae
zoreet <zoreetdotcom@gmail.com>
2019-01-08 15:17:28 +0100

    the buttons work, the date picker works, now I only have to make the data load :DDDD

--- static/js/todayApp.js
+++ static/js/todayApp.js
@@ -10,19 +10,60 @@ let today = {
 
   init: function () {
     let urlParams = new URLSearchParams(window.location.search)
-    let date = null
-    if (urlParams.has('date')) {
-      date = urlParams.get('date')
-    } else {
-      date = moment(Date.now()).format('YYYY-MM-DD')
-    }
 
     app = new Vue({
       el: '#app',
       data: {
         user: this.user,
         token: localStorage.getItem('id_token'),
-        date: date
+        date: moment(Date.now()).format('YYYYMMDD'),
+        today: moment(Date.now()).format('YYYYMMDD'),
+        yesterday: moment(Date.now()).subtract(1, 'days').format('YYYYMMDD'),
+        tomorrow: moment(Date.now()).add(1, 'days').format('YYYYMMDD')
+      },
+      computed: {
+        dateForPicker: {
+          get: function() {
+            return moment(this.date).format('YYYY-MM-DD')
+          },
+          set: function(newDate) {
+            this.date = moment(newDate).format('YYYYMMDD')
+          }
+        },
+        dateTitle () {
+          if (this.date === this.today) {
+            return 'Today'
+          } else if (this.date === this.tomorrow) {
+            return 'Tomorrow'
+          } else if (this.date === this.yesterday) {
+            return 'Yesterday'
+          } else {
+            return moment(this.date).format('DD MMM Y')
+          }
+        },
+        dateSubtitle () {
+          currentDate = moment(this.date)
+          if (this.date === this.today) {
+            return currentDate.format('dddd, DD MMM Y')
+          } else if (this.date === this.tomorrow) {
+            return currentDate.format('dddd, DD MMM Y')
+          } else if (this.date === this.yesterday) {
+            return currentDate.format('dddd, DD MMM Y')
+          } else {
+            return currentDate.format('dddd')
+          }
+        }
+      },
+      methods: {
+        jumpToToday() {
+          this.date = this.today
+        },
+        jumpToTomorrow() {
+          this.date = this.tomorrow
+        },
+        jumpToYesterday() {
+          this.date = this.yesterday
+        }
       }
     })
   }
